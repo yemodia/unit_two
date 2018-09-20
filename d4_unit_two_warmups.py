bf2a4bca7a79a773202615e5d7e2d8bee8dafeb1
Yerim <yerim@iMac6.local>
2018-09-20 09:21:16 -0400

    Mathematical Operators

---
+++ d4_unit_two_warmups.py
@@ -0,0 +1,12 @@
+print(9 * 5)
+print(2 / 5)
+print(12 // 5)
+print(27 // 4)
+print(2 // 5)
+print(5 % 2)
+print(9 % 5)
+print(6 % 6)
+print(2 % 7)
+
+# The // is integer divison meaning the amount of times the number can fully be divided by the other
+# The % is the the remainder left behind after division or modulus operator
